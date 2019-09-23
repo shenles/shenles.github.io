1e1fd50fc05cd0f964bb95c555b7c34afb9b281c
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-09-22 21:36:34 -0700

    visual changes

--- cs/assets/js/main.js
+++ cs/assets/js/main.js
@@ -3,22 +3,15 @@ jQuery(document).ready(function($) {
 
     /*======= Skillset *=======*/
     
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
+    $('.level-bar-inner').each(function() {
         
-            var itemWidth = $(this).data('level');
+        var itemWidth = $(this).data('level');
             
-            $(this).animate({
-                width: itemWidth
-            }, 800);
+        $(this).css('width', itemWidth)
             
-        });
-
     });
-    
+
+
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
     
@@ -59,12 +52,10 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
-    
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    /* Github Calendar */
+    new GitHubCalendar("#github-graph", "shenles");
 
+    /* Github Activity Feed */
+    GitHubActivity.feed({ username: "shenles", selector: "#ghfeed" });
 
 });
