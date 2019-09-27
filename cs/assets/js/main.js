beb57f9e34309d200725aa447bfc5a540561e3e9
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-09-26 19:07:22 -0700

    remove unneeded plugins

--- cs/assets/js/main.js
+++ cs/assets/js/main.js
@@ -55,7 +55,5 @@ jQuery(document).ready(function($) {
     /* Github Calendar */
     new GitHubCalendar("#github-graph", "shenles");
 
-    /* Github Activity Feed */
-    GitHubActivity.feed({ username: "shenles", selector: "#ghfeed" });
 
 });
